d98a3b6904e693efd5c453419d86718e5fa71be0
Kevin Luch <79926777+KevinLuch@users.noreply.github.com>
2021-11-01 07:03:28 -0500

    edited files

---
+++ 7.js
@@ -0,0 +1,14 @@
+// Create a function that finds the maximum range
+// of a triangle's thrid edge, where the side lengths are all
+// integers
+// **************************************************************
+
+function nextEdge(side1, side2) {
+	return (side1 + side2) - 1
+}
+
+// **************************************************************
+
+const nextEdge_v2 = (side1, side2) => side1 + side2 - 1;
+
+// **************************************************************
