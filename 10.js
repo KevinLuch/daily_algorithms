80044af30c043f6e6d1bbcd13d5fbdc29a10dd61
Kevin Luch <79926777+KevinLuch@users.noreply.github.com>
2021-11-06 13:45:52 -0500

    edited files

---
+++ 10.js
@@ -0,0 +1,10 @@
+// Create a function that takes length and width and finds
+// the perimeter of a rectangle
+// **************************************************************
+
+function findPerimeter(length, width) {
+	perimeter = (length + width) * 2
+	return perimeter 
+}
+
+// **************************************************************
