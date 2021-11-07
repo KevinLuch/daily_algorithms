feb4098648d6662b23217c2f0de21bf62b66004c
Kevin Luch <79926777+KevinLuch@users.noreply.github.com>
2021-11-07 07:43:26 -0600

    edited files

---
+++ 12.js
@@ -0,0 +1,26 @@
+// Buggy Code
+// **************************************************************
+
+// function printArray(number) {
+//     var newArray = [];
+
+//     for(var i = 1; i <= number;) {
+//     newArray.push(i);
+//     }
+
+//     return newArray;
+// }
+
+// **************************************************************
+
+function printArray(number) {
+    var newArray = [];
+
+    for(var i = 1; i <= number; i++) {
+    newArray.push(i);
+    }
+
+    return newArray;
+}
+
+// **************************************************************
