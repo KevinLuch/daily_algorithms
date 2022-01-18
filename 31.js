1460b4b007374c5a5592b64cf98469d29bd57b55
Kevin Luch <79926777+KevinLuch@users.noreply.github.com>
2022-01-18 08:21:41 -0600

    edited files

---
+++ 31.js
@@ -0,0 +1,21 @@
+// Given a number, return an array containing the two halves
+// of the number. If the number is odd, make the rightmost number higher.
+
+// Examples:
+// numberSplit(4) ➞ [2, 2]
+// numberSplit(10) ➞ [5, 5]
+// numberSplit(11) ➞ [5, 6]
+// numberSplit(-9) ➞ [-5, -4]
+
+// Notes:
+// All numbers will be integers.
+// You can expect negative numbers too.
+// **************************************************************
+
+function numberSplit(n) {
+	var split = n/2;
+	var arr = [Math.floor(split),Math.ceil(split)];
+	return arr;
+}
+
+// **************************************************************
