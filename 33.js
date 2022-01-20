751a54cbeab33e15afeaa3faaddfd994f66ef2b3
Kevin Luch <79926777+KevinLuch@users.noreply.github.com>
2022-01-20 08:24:26 -0600

    edited files

---
+++ 33.js
@@ -0,0 +1,17 @@
+// Given two arguments, return an array which contains
+// these two arguments
+
+// Examples:
+
+// makePair(1, 2) ➞ [1, 2]
+// makePair(51, 21) ➞ [51, 21]
+// makePair(512124, 215) ➞ [512124, 215]
+// **************************************************************
+
+const makePair = (num1, num2) => {
+    const result = [];
+    result.push(num1, num2);
+    return result;
+}
+
+// **************************************************************
