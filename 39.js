c29ca22896480a9bf29a0e4a59d740d8c67e264f
Kevin Luch <79926777+KevinLuch@users.noreply.github.com>
2022-01-26 06:03:14 -0600

    edited files

---
+++ 39.js
@@ -0,0 +1,19 @@
+// Create a function that takes an integer and returns true
+// if it's divisble by 100, otherwise return false
+
+// Examples:
+
+// - divisible(1) ➞ false
+// - divisible(1000) ➞ true
+// - divisible(100) ➞ true
+
+// **************************************************************
+
+function divisible(num) {
+	if(num % 100 === 0) {
+		return true;
+	}else{
+		return false;
+	}
+}
+// **************************************************************
