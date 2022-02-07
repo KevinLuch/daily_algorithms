1114540b57b6ab0e93abf696e1c6de5fd2653e49
Kevin Luch <79926777+KevinLuch@users.noreply.github.com>
2022-02-07 05:34:20 -0600

    edited files

---
+++ 44.js
@@ -0,0 +1,14 @@
+// Create a function that takes a boolean variable flag and 
+// returns it as a string
+
+// Examples:
+
+// boolToString(true) ➞ "true"
+// boolToString(false) ➞ "false"
+
+// **************************************************************
+
+function boolToString(flag) {
+	return String(flag)
+}
+// **************************************************************
