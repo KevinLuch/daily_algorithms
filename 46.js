1e60ad4b7c4300b7922b3c87c4f701fa19394faa
Kevin Luch <79926777+KevinLuch@users.noreply.github.com>
2022-02-08 05:26:29 -0600

    edited files

---
+++ 46.js
@@ -0,0 +1,19 @@
+// Create a function that return the given argument, but by using
+// an arrow function.
+
+// An arrow function is constructed like so:
+
+// arrowFunc=(/*parameters*/)=>//code here
+
+// Examples:
+
+// arrowFunc(3) ➞ 3
+// arrowFunc("3") ➞ "3"
+// arrowFunc(true) ➞ true
+
+// **************************************************************
+
+arrowFunc=(hello)=> {
+	return hello
+}
+// **************************************************************
