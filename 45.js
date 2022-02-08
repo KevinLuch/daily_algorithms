79f9ea768b013ca9371ac7ae9ebdbe49c8e4ab78
Kevin Luch <79926777+KevinLuch@users.noreply.github.com>
2022-02-07 18:30:18 -0600

    edited files

---
+++ 45.js
@@ -0,0 +1,20 @@
+// Create a function that evaluates an equation.
+
+// Examples:
+
+// eq("1+2") ➞ 3
+// eq("6/(9-7)") ➞ 3
+// eq("3+2-4") ➞ 1
+
+// *** The method used to solve this challenge should not be used
+// in practice. However, it's important to be aware of how this
+// functionality works and why it should not be used.
+
+// https://www.digitalocean.com/community/tutorials/js-eval
+
+// **************************************************************
+
+function eq(evaluate) {
+	return eval(evaluate)
+}
+// **************************************************************
