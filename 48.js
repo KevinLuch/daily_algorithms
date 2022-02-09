cafae36b183c12b3b1a6573da92e4c3aff4cb4db
Kevin Luch <79926777+KevinLuch@users.noreply.github.com>
2022-02-09 04:52:23 -0600

    edited files

---
+++ 48.js
@@ -0,0 +1,19 @@
+// Create a function that returns true if a string is empty
+// and false otherwise.
+
+// Examples:
+
+// isEmpty("") ➞ true
+// isEmpty(" ") ➞ false
+// isEmpty("a") ➞ false
+
+// **************************************************************
+
+function isEmpty(s) {
+	if(s === "") {
+		return true
+	}else{
+		return false
+	}
+}
+// **************************************************************
