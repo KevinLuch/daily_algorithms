e948b4200e812e16baaac7b7358a0dd4d876438b
Kevin Luch <79926777+KevinLuch@users.noreply.github.com>
2022-02-10 04:54:59 -0600

    edited files

---
+++ 49.js
@@ -0,0 +1,26 @@
+// Write a function that checks whether a person can watch an
+// MA15+ rated movie. One of the following two conditions is required
+// for admittance:
+
+// The person is at least 15 years old.
+// They have parental supervision.
+
+// The function accpets two parameters, age, and isSupervised.
+// Return a boolen
+
+// Examples:
+
+// acceptIntoMovie(14, true) ➞ true
+// acceptIntoMovie(14, false) ➞ false
+// acceptIntoMovie(16, false) ➞ true
+
+// **************************************************************
+
+function acceptIntoMovie(age, isSupervised) {
+    if (age>=15 || isSupervised){
+        return true
+    } else {
+        return false
+    }
+}
+// **************************************************************
