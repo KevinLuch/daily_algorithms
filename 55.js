3f4f224d332a53f38f492a690b765c9578d79066
Kevin Luch <79926777+KevinLuch@users.noreply.github.com>
2022-02-16 07:27:09 -0600

    edited files

---
+++ 55.js
@@ -0,0 +1,33 @@
+// Create a function that takes an array of numbers and return
+// both the minimum and maximum numbers, in that order
+
+// Examples:
+
+// minMax([1, 2, 3, 4, 5]) ➞ [1, 5]
+// minMax([2334454, 5]) ➞ [5, 2334454]
+// minMax([1]) ➞ [1, 1]
+
+// **************************************************************
+
+function minMax(arr) {
+    var returnArr = [];
+    var min = arr[0];
+    var max = arr[0];
+    
+    for(var i = 1; i < arr.length; i++) {
+
+    if(arr[i] < min) {
+        min = arr[i];
+    }
+    if(arr[i] > max) {
+        max = arr[i];
+    }
+    }
+    
+    returnArr.push(min);
+    returnArr.push(max);
+    
+    return returnArr;
+}
+
+// **************************************************************
